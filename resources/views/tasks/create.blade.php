00ab9acd37d34fd87acbcc0e382df2279f6d3930
Hoshito823 <otihsoh823@gmail.com>
2019-11-10 00:27:57 +0000

    commit for first setup

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>ToDoList</title>
+    </head>
+    <body>
+        <h1>Hey this is my Todo List resistraion page.</h1>
+    </body>
+</html>
