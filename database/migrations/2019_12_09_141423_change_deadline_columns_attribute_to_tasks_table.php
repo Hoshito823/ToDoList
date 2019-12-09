be1aa41a09bb90aad3620a8986cb41814b5d8775
Hoshito823 <otihsoh823@gmail.com>
2019-12-09 14:17:11 +0000

    commit for fixing heroku error

--- database/migrations/2019_12_09_141423_change_deadline_columns_attribute_to_tasks_table.php
+++ database/migrations/2019_12_09_141423_change_deadline_columns_attribute_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddDeadlineToTasksTable extends Migration
+class ChangeDeadlineColumnsAttributeToTasksTable extends Migration
 {
     /**
      * Run the migrations.
