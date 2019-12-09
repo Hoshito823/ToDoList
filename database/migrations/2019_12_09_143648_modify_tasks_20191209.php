5429592ccc295a505efadd239218122c8333f7c9
Hoshito823 <otihsoh823@gmail.com>
2019-12-09 14:39:14 +0000

    commit for fix error of heroku

--- database/migrations/2019_12_09_143648_modify_tasks_20191209.php
+++ database/migrations/2019_12_09_143648_modify_tasks_20191209.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeDeadlineColumnsAttributeToTasksTable extends Migration
+class ModifyTasks20191209 extends Migration
 {
     /**
      * Run the migrations.
